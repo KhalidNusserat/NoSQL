eebc4ce04101720e44c6c757b6ad16c0cda5a8be
Khalid <KhMKhNu@gmail.com>
2022-05-22 22:54:51 +0300

    added Document.hash and Document.id, and fixed Document.builder() not being static

--- src/main/java/com/atypon/document/Document.java
+++ src/main/java/com/atypon/document/Document.java
@@ -1,38 +1,61 @@
 package com.atypon.document;
 
+import com.google.common.hash.Hashing;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class Document implements DocumentValue {
-    private final ConcurrentMap<DocumentField, DocumentValue> fields;
+    private final String id;
 
-    private Document() {
-        fields = new ConcurrentHashMap<>();
-    }
+    private final int hash;
+
+    private final ConcurrentMap<DocumentField, DocumentValue> fields;
 
     private Document(Map<DocumentField, DocumentValue> fields) {
         this.fields = new ConcurrentHashMap<>(fields);
+        this.hash = Objects.hashCode(fields);
+        this.id = Hashing.sha256().hashInt(hash).toString();
     }
 
     private DocumentValue get(DocumentField field) {
         return fields.get(field);
     }
 
-    public DocumentBuilder builder() {
+    public static DocumentBuilder builder() {
         return new DocumentBuilder();
+    }
+
+    public String getId() {
+        return id;
     }
 
     public static class DocumentBuilder {
         private final Map<DocumentField, DocumentValue> fields = new HashMap<>();
 
-        public void put(DocumentField field, DocumentValue value) {
+        public DocumentBuilder put(DocumentField field, DocumentValue value) {
             fields.put(field, value);
+            return this;
         }
 
         public Document build() {
             return new Document(fields);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Document document = (Document) o;
+        return hash == document.hash;
+    }
+
+    @Override
+    public int hashCode() {
+        return hash;
     }
 }
