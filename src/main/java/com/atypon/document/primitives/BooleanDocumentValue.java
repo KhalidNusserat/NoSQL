fb9d6267886ee2c6a43fd9318f704d698b4053ee
Khalid <KhMKhNu@gmail.com>
2022-05-22 21:00:07 +0300

    created BooleanDocumentValue

---
+++ src/main/java/com/atypon/document/primitives/BooleanDocumentValue.java
@@ -0,0 +1,31 @@
+package com.atypon.document.primitives;
+
+import com.atypon.document.DocumentValue;
+
+import java.util.Objects;
+
+public record BooleanDocumentValue(boolean value) implements DocumentValue {
+    public static BooleanDocumentValue fromBoolean(boolean value) {
+        return new BooleanDocumentValue(value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BooleanDocumentValue that = (BooleanDocumentValue) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "BooleanDocumentValue{" +
+                "value=" + value +
+                '}';
+    }
+}
