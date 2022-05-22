f59c2a932912aef4433b9da37122dc026f306e19
Khalid <KhMKhNu@gmail.com>
2022-05-22 21:44:54 +0300

    added a get method and an iterator to ArrayDocumentValue

--- src/main/java/com/atypon/document/primitives/ArrayDocumentValue.java
+++ src/main/java/com/atypon/document/primitives/ArrayDocumentValue.java
@@ -3,9 +3,10 @@ package com.atypon.document.primitives;
 import com.atypon.document.DocumentValue;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class ArrayDocumentValue<T extends DocumentValue> implements DocumentValue {
+public class ArrayDocumentValue<T extends DocumentValue> implements DocumentValue, Iterable<T> {
     private final List<T> list;
 
     public ArrayDocumentValue() {
@@ -18,5 +19,20 @@ public class ArrayDocumentValue<T extends DocumentValue> implements DocumentValu
 
     public static <T extends DocumentValue> ArrayDocumentValue<T> fromList(List<T> list) {
         return new ArrayDocumentValue<>(list);
+    }
+
+    public T get(int index) {
+        return list.get(index);
+    }
+
+    public ArrayDocumentValue<T> addAll(List<T> list) {
+        List<T> newList = new ArrayList<>(list);
+        newList.addAll(this.list);
+        return new ArrayDocumentValue<>(newList);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return list.iterator();
     }
 }
