2b5d4a3e5a40995c963f22b88b4312905985620b
Khalid <KhMKhNu@gmail.com>
2022-05-22 20:58:45 +0300

    created DoubleDocumentValue

---
+++ src/main/java/com/atypon/document/primitives/DoubleDocumentValue.java
@@ -0,0 +1,31 @@
+package com.atypon.document.primitives;
+
+import com.atypon.document.DocumentValue;
+
+import java.util.Objects;
+
+public record DoubleDocumentValue(double value) implements DocumentValue {
+    public static DoubleDocumentValue fromDouble(double value) {
+        return new DoubleDocumentValue(value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DoubleDocumentValue that = (DoubleDocumentValue) o;
+        return Double.compare(that.value, value) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "DoubleDocumentValue{" +
+                "value=" + value +
+                '}';
+    }
+}
