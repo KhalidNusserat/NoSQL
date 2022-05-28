860c3d2447c28d5e4aed3c1351f56126d8b15ddf
Khalid <KhMKhNu@gmail.com>
2022-05-28 15:01:07 +0300

    removed AliasNotFoundException

--- src/main/java/com/atypon/nosql/store/AliasNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.atypon.nosql.store;
-
-public class AliasNotFoundException extends StoreException {
-    public AliasNotFoundException(String message) {
-        super(message);
-    }
-}
