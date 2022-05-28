32ee824a114f96bf0830eaabc6b85a038c68ff2f
Khalid <KhMKhNu@gmail.com>
2022-05-28 15:02:05 +0300

    created FilesCreator

---
+++ src/main/java/com/atypon/nosql/store/FilesCreator.java
@@ -0,0 +1,17 @@
+package com.atypon.nosql.store;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FilesCreator {
+    public static void createDirectory(String directory) {
+        boolean ignored = new File(directory).mkdirs();
+    }
+
+    public static void create(String filepath, String filename) {
+        createDirectory(filepath);
+        try {
+            boolean ignored = new File(filepath + filename).createNewFile();
+        } catch (IOException ignored) {}
+    }
+}
