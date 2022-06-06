fb31f05fd0f2e3e60bccbd0093157c7cc0011e56
Khalid <KhMKhNu@gmail.com>
2022-06-06 16:41:26 +0300

    made CopyOnWriteIO generic

--- src/main/java/com/atypon/nosql/io/CopyOnWriteIO.java
+++ src/main/java/com/atypon/nosql/io/CopyOnWriteIO.java
@@ -3,12 +3,12 @@ package com.atypon.nosql.io;
 import java.io.IOException;
 import java.nio.file.Path;
 
-public interface CopyOnWriteIO {
-    <T> Path write(T file, Path directory, String extension) throws IOException;
+public interface CopyOnWriteIO<T> {
+    Path write(T file, Path directory, String extension) throws IOException;
 
-    <T> T read(Path filepath, Class<T> tClass) throws IOException;
+    T read(Path filepath) throws IOException;
 
     void delete(Path filepath);
 
-    <T> Path update(T newFile, Path filepath, String extension) throws IOException;
+    Path update(T newFile, Path filepath, String extension) throws IOException;
 }
--- src/main/java/com/atypon/nosql/io/GsonCopyOnWriteIO.java
+++ src/main/java/com/atypon/nosql/io/GsonCopyOnWriteIO.java
@@ -1,5 +1,6 @@
 package com.atypon.nosql.io;
 
+import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 
 import java.io.BufferedReader;
@@ -11,7 +12,7 @@ import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class GsonCopyOnWriteIO implements CopyOnWriteIO {
+public class GsonCopyOnWriteIO<T> implements CopyOnWriteIO<T> {
     private final ExecutorService deleteService = Executors.newCachedThreadPool();
 
     private final Gson gson = new Gson();
@@ -19,7 +20,7 @@ public class GsonCopyOnWriteIO implements CopyOnWriteIO {
     private final Random random = new Random();
 
     @Override
-    public <T> Path write(T file, Path directory, String extension) throws IOException {
+    public Path write(T file, Path directory, String extension) throws IOException {
         Path filepath = directory.resolve(random.nextLong() + extension);
         try (BufferedWriter writer = Files.newBufferedWriter(filepath)) {
             gson.toJson(file, writer);
@@ -28,9 +29,9 @@ public class GsonCopyOnWriteIO implements CopyOnWriteIO {
     }
 
     @Override
-    public <T> T read(Path filepath, Class<T> tClass) throws IOException {
+    public T read(Path filepath) throws IOException {
         try (BufferedReader reader = Files.newBufferedReader(filepath)) {
-            return gson.fromJson(reader, tClass);
+            return gson.fromJson(reader, new TypeToken<T>(){}.getType());
         }
     }
 
@@ -40,7 +41,7 @@ public class GsonCopyOnWriteIO implements CopyOnWriteIO {
     }
 
     @Override
-    public <T> Path update(T newFile, Path filepath, String extension) throws IOException {
+    public Path update(T newFile, Path filepath, String extension) throws IOException {
         Path newFilepath = write(newFile, filepath.getParent(), extension);
         delete(filepath);
         return newFilepath;
