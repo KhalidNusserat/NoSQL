5c8cf53eb094f98078066efe72f5b32a65cdd35f
Khalid <KhMKhNu@gmail.com>
2022-06-08 10:00:21 +0300

    renamed the argument bound in DocumentsCollection match methods to documentMatcher, for consistency

--- src/main/java/com/atypon/nosql/collection/DocumentsCollection.java
+++ src/main/java/com/atypon/nosql/collection/DocumentsCollection.java
@@ -7,13 +7,13 @@ import java.io.IOException;
 import java.util.Collection;
 
 public interface DocumentsCollection<T extends Document<?>> {
-    boolean contains(T bound) throws IOException;
+    boolean contains(T matchDocument) throws IOException;
 
-    Collection<T> get(T bound) throws IOException;
+    Collection<T> get(T matchDocument) throws IOException;
 
     void put(T document) throws IOException, SchemaViolationException;
 
-    void remove(T bound) throws IOException;
+    void remove(T matchDocument) throws IOException;
 
     Collection<T> getAll() throws IOException;
 }
