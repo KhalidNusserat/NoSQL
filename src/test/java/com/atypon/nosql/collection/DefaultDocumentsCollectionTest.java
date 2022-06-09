bc7e6005187bc157a05ba74d5bee984d054eeff7
Khalid <KhMKhNu@gmail.com>
2022-06-09 22:31:40 +0300

    fixed generics

--- src/test/java/com/atypon/nosql/collection/DefaultDocumentsCollectionTest.java
+++ src/test/java/com/atypon/nosql/collection/DefaultDocumentsCollectionTest.java
@@ -1,12 +1,10 @@
 package com.atypon.nosql.collection;
 
 import com.atypon.nosql.gsondocument.GsonDocument;
-import com.google.gson.JsonElement;
 
-class DefaultDocumentsCollectionTest extends DocumentsCollectionTest<DefaultDocumentsCollection<JsonElement, GsonDocument>> {
-
+class DefaultDocumentsCollectionTest extends DocumentsCollectionTest<DefaultDocumentsCollection<GsonDocument>> {
     @Override
-    public DefaultDocumentsCollection<JsonElement, GsonDocument> create() {
+    public DefaultDocumentsCollection<GsonDocument> create() {
         return DefaultDocumentsCollection.from(documentsIO, testDirectory);
     }
 }
