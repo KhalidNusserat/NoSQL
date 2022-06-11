cf4ed0a69517b4611b4cdcc401de0c0fec9acd2e
Khalid <KhMKhNu@gmail.com>
2022-06-11 18:47:55 +0300

    deleted unnecessary classes

--- src/main/java/com/atypon/nosql/gsondocument/GsonElementSchema.java
+++
@@ -1,7 +0,0 @@
-package com.atypon.nosql.gsondocument;
-
-import com.atypon.nosql.schema.ElementSchema;
-import com.google.gson.JsonElement;
-
-public interface GsonElementSchema extends ElementSchema<JsonElement> {
-}
--- src/main/java/com/atypon/nosql/schema/DocumentSchema.java
+++
@@ -1,9 +0,0 @@
-package com.atypon.nosql.schema;
-
-import com.atypon.nosql.document.Document;
-
-import javax.naming.directory.SchemaViolationException;
-
-public interface DocumentSchema<T extends Document<?>> {
-    T makeDocumentValid(T document) throws SchemaViolationException;
-}
