19ad7fde82c80e183af2c1cad2bbe28ebfe8a952
Khalid <KhMKhNu@gmail.com>
2022-06-15 19:44:39 +0300

    added a builder to GenericDatabaseGenerator

--- src/main/java/com/atypon/nosql/database/GenericDatabaseGenerator.java
+++ src/main/java/com/atypon/nosql/database/GenericDatabaseGenerator.java
@@ -14,22 +14,63 @@ public class GenericDatabaseGenerator<T extends Document<?>> implements Database
 
     private final DocumentSchemaGenerator<T> schemaGenerator;
 
-    public GenericDatabaseGenerator(
+    private final Path databasesDirectory;
+
+    private GenericDatabaseGenerator(
             IOEngine<T> ioEngine,
             DocumentGenerator<T> documentGenerator,
-            DocumentSchemaGenerator<T> schemaGenerator) {
+            DocumentSchemaGenerator<T> schemaGenerator, Path databasesDirectory) {
         this.ioEngine = ioEngine;
         this.documentGenerator = documentGenerator;
         this.schemaGenerator = schemaGenerator;
+        this.databasesDirectory = databasesDirectory;
+    }
+
+    public static <T extends Document<?>> GenericDatabaseGeneratorBuilder<T> builder() {
+        return new GenericDatabaseGeneratorBuilder<>();
     }
 
     @Override
     public Database create(Path databaseDirectory) {
         return GenericDatabase.<T>builder()
-                .setDatabaseDirectory(databaseDirectory)
+                .setDatabaseDirectory(databasesDirectory.resolve(databaseDirectory))
                 .setDocumentGenerator(documentGenerator)
                 .setDocumentSchemaGenerator(schemaGenerator)
                 .setIoEngine(ioEngine)
                 .build();
+    }
+
+    public static class GenericDatabaseGeneratorBuilder<T extends Document<?>> {
+        private IOEngine<T> ioEngine;
+
+        private DocumentGenerator<T> documentGenerator;
+
+        private DocumentSchemaGenerator<T> schemaGenerator;
+
+        private Path databasesDirectory;
+
+        public GenericDatabaseGeneratorBuilder<T> setIoEngine(IOEngine<T> ioEngine) {
+            this.ioEngine = ioEngine;
+            return this;
+        }
+
+        public GenericDatabaseGeneratorBuilder<T> setDocumentGenerator(DocumentGenerator<T> documentGenerator) {
+            this.documentGenerator = documentGenerator;
+            return this;
+        }
+
+        public GenericDatabaseGeneratorBuilder<T> setSchemaGenerator(DocumentSchemaGenerator<T> schemaGenerator) {
+            this.schemaGenerator = schemaGenerator;
+            return this;
+        }
+
+        public GenericDatabaseGeneratorBuilder<T> setDatabasesDirectory(Path databasesDirectory) {
+            this.databasesDirectory = databasesDirectory;
+            return this;
+        }
+
+        public GenericDatabaseGenerator<T> build() {
+            return new GenericDatabaseGenerator<>(ioEngine, documentGenerator, schemaGenerator, databasesDirectory);
+        }
     }
 }
