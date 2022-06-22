84d30605e7997b00398c7787eddc972b1d303f54
Khalid <KhMKhNu@gmail.com>
2022-06-22 17:23:17 +0300

    added schema to payload

--- src/main/java/com/atypon/nosql/databaserequest/Payload.java
+++ src/main/java/com/atypon/nosql/databaserequest/Payload.java
@@ -6,17 +6,24 @@ import java.util.Map;
 public record Payload(Map<String, Object> criteria,
                       List<Map<String, Object>> documents,
                       Map<String, Object> index,
-                      Map<String, Object> update) {
+                      Map<String, Object> update,
+                      Map<String, Object> schema) {
 
     public static PayloadBuilder builder() {
         return new PayloadBuilder();
     }
 
     public static class PayloadBuilder {
+
         private Map<String, Object> criteria;
+
         private List<Map<String, Object>> documents;
+
         private Map<String, Object> index;
+
         private Map<String, Object> update;
+
+        private Map<String, Object> schema;
 
         public PayloadBuilder setCriteria(Map<String, Object> criteria) {
             this.criteria = criteria;
@@ -38,8 +45,13 @@ public record Payload(Map<String, Object> criteria,
             return this;
         }
 
+        public PayloadBuilder setSchema(Map<String, Object> schema) {
+            this.schema = schema;
+            return this;
+        }
+
         public Payload createPayload() {
-            return new Payload(criteria, documents, index, update);
+            return new Payload(criteria, documents, index, update, schema);
         }
     }
 }
