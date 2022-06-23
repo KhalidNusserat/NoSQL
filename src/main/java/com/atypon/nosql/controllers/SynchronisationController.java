51c2c661c96baa4ad372a9c9e5c59b001ce0a6c8
Khalid <KhMKhNu@gmail.com>
2022-06-23 19:19:27 +0300

    created SynchronisationController

---
+++ src/main/java/com/atypon/nosql/controllers/SynchronisationController.java
@@ -0,0 +1,23 @@
+package com.atypon.nosql.controllers;
+
+import com.atypon.nosql.databaserequest.DatabaseRequest;
+import com.atypon.nosql.requesthandlers.DatabaseRequestHandler;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SynchronisationController {
+
+    private final DatabaseRequestHandler requestHandler;
+
+    public SynchronisationController(@Qualifier("defaultHandler") DatabaseRequestHandler requestHandler) {
+        this.requestHandler = requestHandler;
+    }
+
+    @PostMapping("/sync")
+    private void synchronise(@RequestBody DatabaseRequest request) {
+        requestHandler.handle(request);
+    }
+}
