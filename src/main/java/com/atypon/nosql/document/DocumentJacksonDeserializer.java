87c4a3185eeb548721e3c622eb73381b4722f201
Khalid <KhMKhNu@gmail.com>
2022-06-26 19:56:46 +0300

    deleted redundant RuntimeTypeAdapterFactory, and made DocumentJacksonDeserializer type-safe

--- src/main/java/com/atypon/nosql/document/DocumentJacksonDeserializer.java
+++ src/main/java/com/atypon/nosql/document/DocumentJacksonDeserializer.java
@@ -2,6 +2,7 @@ package com.atypon.nosql.document;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
 import java.io.IOException;
@@ -9,11 +10,15 @@ import java.util.Map;
 
 public class DocumentJacksonDeserializer extends JsonDeserializer<Document> {
 
-    @SuppressWarnings("unchecked")
     @Override
     public Document deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
             throws IOException {
-        Map<String, Object> documentMap = jsonParser.readValueAs(Map.class);
+        JavaType mapType = deserializationContext.getTypeFactory().constructMapType(
+                Map.class,
+                String.class,
+                Object.class
+        );
+        Map<String, Object> documentMap = deserializationContext.readValue(jsonParser, mapType);
         return Document.fromMap(documentMap);
     }
 }
